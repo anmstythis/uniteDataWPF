d66a17473461c4e4cc1f7adf0c3179b4518e5356
anmstythis <isip_a.a.gladkova@mpt.ru>
2024-03-18 19:49:16 +0300

    entity framework

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3_entity_framework
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        neoCorpEntities neodb = new neoCorpEntities();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            neoDgr.ItemsSource = neodb.staff.ToList();    
+        }
+    }
+}
